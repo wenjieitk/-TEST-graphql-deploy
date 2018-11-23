a404fd4a96f6885a33fadc58961be2ba0247e719
Wen Jie <wenjie.sing@laputa.com.my>
2018-11-23 15:39:44 +0800

    setting up environment variables

--- src/prisma.js
+++ src/prisma.js
@@ -3,7 +3,7 @@ import {Prisma} from 'prisma-binding';
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
     endpoint: process.env.PRISMA_ENDPOINT,
-    secret: 'mysecret'
+    secret: process.env.PRISMA_SECRET
 });
 
 export {prisma as default}
