dada7913c423f9961d0c8cacc10af754b483a2d3
Wen Jie <wenjiens15@gmail.com>
2018-11-26 01:35:40 +0800

    testing

---
+++ test/user.test.js
@@ -0,0 +1,31 @@
+import { getFirstName, isValidPassword } from '../src/utils/user';
+
+test('Should return first name when given full name', () => {
+    const firstName = getFirstName('Andrew Mead')
+
+    expect(firstName).toBe('Andrew')
+})
+
+test('Should return first name when given first name', () => {
+    const firstName = getFirstName('Jen')
+
+    expect(firstName).toBe('Jen')
+})
+
+test('Should reject password shorter than 8 characters', () => {
+    const isValid = isValidPassword('abc123')
+
+    expect(isValid).toBe(false)
+})
+
+test('Should reject password that contains word password', () => {
+    const isValid = isValidPassword('abcPassword098')
+
+    expect(isValid).toBe(false)
+})
+
+test('Should correctly validate a valid password', () => {
+    const isValid = isValidPassword('Test123098!')
+
+    expect(isValid).toBe(true)
+})
