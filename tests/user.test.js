4b5c1b073d1f6975896728230d60d2bb92ced702
Wen Jie <wenjie.sing@laputa.com.my>
2018-11-26 18:51:22 +0800

    configure jest to start graphql

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,31 +1,31 @@
-import { getFirstName, isValidPassword } from '../src/utils/user';
+import 'cross-fetch/polyfill'
+import ApolloBoost, {gql} from 'apollo-boost'
 
-test('Should return first name when given full name', () => {
-    const firstName = getFirstName('Andrew Mead')
-
-    expect(firstName).toBe('Andrew')
+const client = new ApolloBoost ({
+    uri: 'http://localhost:4000'
 })
 
-test('Should return first name when given first name', () => {
-    const firstName = getFirstName('Jen')
+test('should create a new user', async() => {
+    const createUser = gql`
+        mutation {
+            createUser(
+                data: {
+                    name: "from Test 1",
+                    email: "fromTest1@gmail.com",
+                    password: "123123123"
+                }
+            ){
+                token,
+                user{
+                    id
+                    name
+                    email
+                }
+            }
+        }
+    `
 
-    expect(firstName).toBe('Jen')
-})
-
-test('Should reject password shorter than 8 characters', () => {
-    const isValid = isValidPassword('abc123')
-
-    expect(isValid).toBe(false)
-})
-
-test('Should reject password that contains word password', () => {
-    const isValid = isValidPassword('abcPassword098')
-
-    expect(isValid).toBe(false)
-})
-
-test('Should correctly validate a valid password', () => {
-    const isValid = isValidPassword('Test123098!')
-
-    expect(isValid).toBe(true)
-})
+    const response = await client.mutate({
+        mutation: createUser
+    })
+});
