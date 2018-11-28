a22c051a31216f91173d96e064aafeac46c3dd15
Wen Jie <wenjie.sing@laputa.com.my>
2018-11-28 10:30:32 +0800

    seeding test database

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,9 +1,51 @@
 import 'cross-fetch/polyfill'
 import ApolloBoost, {gql} from 'apollo-boost'
 import prisma from '../src/prisma'
+import bcrypt from 'bcryptjs';
 
 const client = new ApolloBoost ({
     uri: 'http://localhost:4000'
+})
+
+beforeEach(async () => {
+    jest.setTimeout(10000)
+    // delete all the posts before each test
+    await prisma.mutation.deleteManyPosts()
+    // delete all the users before each test
+    await prisma.mutation.deleteManyUsers()
+    // create a user before each test
+    const user = await prisma.mutation.createUser({
+        data: {
+            name: "From Test",
+            email: "FromTest@gmail.com",
+            password: bcrypt.hashSync('123123123',10)
+        }
+    })
+    // create a post before each test
+    await prisma.mutation.createPost({
+        data: {
+            title: `Test post`,
+            body: `testing body`,
+            published: true,
+            author: {
+                connect: {
+                    id: user.id
+                }
+            }
+        }
+    })
+    await prisma.mutation.createPost({
+        data: {
+            title: `Test post 2`,
+            body: `testing body 2`,
+            published: false,
+            author: {
+                connect: {
+                    id: user.id
+                }
+            }
+        }
+    })
 })
 
 test('should create a new user', async() => {
