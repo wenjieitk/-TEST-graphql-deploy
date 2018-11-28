e34a6a512e2a039bc1a5a1f3943840c4dd4db8c5
Wen Jie <wenjie.sing@laputa.com.my>
2018-11-28 11:25:24 +0800

    test suites refactor 2

---
+++ tests/utils/getClient.js
@@ -0,0 +1,11 @@
+import ApolloBoost from 'apollo-boost'
+
+const getClient = () => {
+    return new ApolloBoost ({
+        uri: 'http://localhost:4000'
+    })
+}
+
+export {
+    getClient as default
+}
